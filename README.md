fe0f44d068f5b6d134d89b4313bd4844094fdb86
Talal Abdelaziz <talal.abdelaziz@gmail.com>
2019-07-06 13:49:25 -0500

    Filled in some wrappers

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 Documentation is Teal's job, I'm outta here
+
+So far, this logs into Mibbit and navigates to the logs tab. It doesn't download logs yet.
+
+This requires a few things:
+
+- chromedriver in the PATH (on Windows, just plop chromedriver into the same folder as logboi.py)
+
+- A properties.py file in the following format:
+ON LINE 1: username = "(your username)"
+ON LINE 2: password = "(your password)"
+
+As long as there are no nickname conflicts, it should go right to the log tab. From there we have to figure out how to iterate through the various channels and dates. Challenges here include:
+- the fact that a "DELETE ALL LOGS FOR THIS CHANNEL" button is right next to the channel name, for some reason
+- the links aren't arranged in a proper list
--- logboi.py
+++ logboi.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python
+import time
 from selenium import webdriver
 # We'll probably find other pieces of selenium useful, but for now that's fine
 import properties
+import xpaths
 
 
 class MibbitBrowser:
     def __init__(self):
-        pass
+        self.driver = webdriver.Chrome()
 
-    def load_webdriver(self):
-        pass
+    def load_website(self):
+        self.driver.get("https://client00.chat.mibbit.com/")
+        # It may be useful to add code to try out client01 and client02
+        # if this fails
 
     def log_in_to_mibbit(self):
-        pass
+        login_link = self.driver.find_element_by_xpath(xpaths.login_link)
+        login_link.click()
+        time.sleep(1)  # Give the modal a second to come into view
+        self.driver.switch_to.frame(self.driver.find_element_by_xpath(
+            xpaths.login_frame))
+        username_input_box = self.driver.find_element_by_xpath(xpaths.username)
+        username_input_box.send_keys(properties.username)
+        password_input_box = self.driver.find_element_by_xpath(xpaths.password)
+        password_input_box.send_keys(properties.password)
+        login_button = self.driver.find_element_by_xpath(xpaths.login_button)
+        login_button.click()
+        time.sleep(3)  # Mibbit needs time for the login to actually occur
+        self.driver.switch_to.default_content()
 
     def navigate_to_logs_page(self):
-        pass
+        logs_panel_link = self.driver.find_element_by_xpath(
+            xpaths.logs_panel_link)
+        logs_panel_link.click()
+        logs_tab = self.driver.find_element_by_xpath(xpaths.logs_tab)
+        logs_tab.click()
+        self.driver.switch_to.frame(self.driver.find_element_by_xpath(
+            xpaths.logs_frame))
+        channel_logs_link = self.driver.find_element_by_xpath(
+            xpaths.channel_logs_link)
+        channel_logs_link.click()
 
-    def find_log_element(self):
-        pass
-
-    def copy_log_to_file(self):
+    def copy_logs_to_files(self):
         pass
 
     def turn_on_logboi(self):
-        self.load_webdriver()
+        self.load_website()
         self.log_in_to_mibbit()
         self.navigate_to_logs_page()
-        # Run the next parts in a loop to copy every log, probably with some
-        # arguments
-        self.find_log_element()
-        self.copy_log_to_file()
+        self.copy_logs_to_files()
 
 
 m = MibbitBrowser()
